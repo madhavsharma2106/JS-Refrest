b1719195bc707a613fb70a5cc42053fa45ab4562
madhavsharma2106 <madhavsharma2106@gmail.com>
2018-06-01 18:16:10 +0530

    Event Bubbling

---
+++ event-bubbling/main.js
@@ -0,0 +1,19 @@
+document.querySelector('.five').addEventListener('click', ()=>{
+    console.log("five");
+});
+
+document.querySelector('.four').addEventListener('click', ()=>{
+    console.log("four");
+});
+
+document.querySelector('.three').addEventListener('click', ()=>{
+    console.log("three");
+});
+
+document.querySelector('.two').addEventListener('click', ()=>{
+    console.log("two");
+});
+
+document.querySelector('.one').addEventListener('click', ()=>{
+    console.log("one");
+});
